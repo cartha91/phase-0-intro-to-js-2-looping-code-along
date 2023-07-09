ea66925f8854eea1a129e8636fec2fc3dc94c0ae
steven dulworth <crowscoupetech@proton.me>
2023-07-09 18:30:12 -0500

    3 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    
+    let messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      
+      let name = names[i];
+      
+      let message = `Thank you, ${name}, for the wonderful ${event} gift!`;
+      
+      messages.push(message);
+    }
+    
+    return messages;
+  }
+
+  function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number--; 
+    }
+  }
